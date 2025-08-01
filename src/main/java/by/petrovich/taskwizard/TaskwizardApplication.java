dc57d0604cf81c00cbe5f167916c88ad841000ed
Petrovich Alexandr <A.Piatrovich@gmail.com>
2025-08-01 11:30:21 +0300

    Initial commit

---
+++ src/main/java/by/petrovich/taskwizard/TaskwizardApplication.java
@@ -0,0 +1,13 @@
+package by.petrovich.taskwizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskwizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskwizardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/petrovich/taskwizard/TaskwizardApplicationTests.java
@@ -0,0 +1,13 @@
+package by.petrovich.taskwizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskwizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
