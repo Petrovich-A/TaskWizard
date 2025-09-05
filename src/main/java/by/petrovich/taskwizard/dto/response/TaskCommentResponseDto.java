e0d50b987d02f507eb9a6884e97fd9866c4ac20f
Petrovich Alexandr <A.Piatrovich@gmail.com>
2025-09-05 11:56:35 +0300

    feat(test): added TaskController integration tests for testing getting pageable tasks list

--- src/main/java/by/petrovich/taskwizard/dto/response/TaskCommentResponseDto.java
+++ src/main/java/by/petrovich/taskwizard/dto/response/TaskCommentResponseDto.java
@@ -1,17 +1,25 @@
 package by.petrovich.taskwizard.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+import static by.petrovich.taskwizard.constant.Constant.DATE_TIME_FORMAT_PATTERN;
+
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskCommentResponseDto {
     private Long id;
 
     private String comment;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_TIME_FORMAT_PATTERN)
     private LocalDateTime createdAt;
 
     private String author;
